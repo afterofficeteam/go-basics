99da26d24cc2b43c27045084c6d1ff8adb616f0d
Erwin <erwin011895@gmail.com>
2024-08-14 04:16:24 +0700

    fix: concurrent waitgroup

--- concurrent/main.go
+++ concurrent/main.go
@@ -23,11 +23,13 @@ var URLs = []string{
 }
 
 func main() {
-	defer fmt.Println("main thread end")
+	// defer punya internal stack
+	defer fmt.Println("main goroutine end") // close program
 	defer func() {
-		fmt.Println("defer")
+		// fmt.Println("defer") // close resource connection sql
 	}()
-	fmt.Println("main thread start")
+	// defer fmt.Println("ke 3") // close channel
+	fmt.Println("main goroutine start")
 
 	// basicGoroutine()
 	// basicChan()
@@ -37,16 +39,20 @@ func main() {
 	// fetchSequentially()
 	// fmt.Println("")
 	// fetchConcurrently()
+	// fmt.Println("")
+	// fetchConcurrentlyWaitgroup()
 }
 
 func basicGoroutine() {
+	fmt.Println("basicGoroutine")
 	// spawn 1 goroutine with "anonymous func" / Closure
 	go func() {
 		fmt.Println("anonymous goroutine")
-	}()
+	}() // JS IIFE
 
 	// spawn 1 goroutine that run fmt.Println()
 	go fmt.Println("hello")
+	// go basicChan()
 
 	// spawn 10 goroutine that each goroutine will run fmt.Println() 0 to 9 respectively
 	for i := 0; i < 10; i++ {
@@ -57,33 +63,35 @@ func basicGoroutine() {
 
 func basicChan() {
 	/* UNBUFFERED CHANNEL */
-	messageChan := make(chan string) // unbuffered channel
+	messageChan := make(chan string) // unbuffered channel, bisa terima unlimited message
 	go func() {
-		messageChan <- "tes" // pass "tes" to messageChan
+		messageChan <- "tes" // send "tes" to messageChan
 	}()
-	msg := <-messageChan // output 1 data from messageChan // BLOCKING
-	fmt.Println(msg)
-	// msg = <-messageChan // Blocking main goroutine, waiting for new data from messageChan
+	msg := <-messageChan // output 1 data from messageChan. possible BLOCKING when no data in messageChan
+	fmt.Println("msg", msg)
+	// msg = <-messageChan // uncomment this will BLOCK main goroutine, waiting for new data from messageChan
+	// return
 
 	/* BUFFERED CHANNEL */
-	bufferedMessageChan := make(chan string, 2)
+	bufferedMessageChan := make(chan string, 2) // maksimal 2 message di dalam chan dalam 1 waktu
 
 	go func() {
 		for i := 1; i <= 10; i++ {
 			bufferedMessageChan <- fmt.Sprintf("data %d", i)
 			fmt.Println("done sending data", i)
-			time.Sleep(time.Second)
+			// time.Sleep(time.Second)
 		}
 		close(bufferedMessageChan)
 	}()
 
-	// loop over channel. quit loop when channel is CLOSED
+	// range over channel. quit loop when channel is CLOSED
 	for str := range bufferedMessageChan {
 		fmt.Println("received", str)
-		// time.Sleep(time.Second)
+		time.Sleep(time.Second)
 	}
 
 	/* CHANNEL DIRECTION in func */
+	// data angka 2 => chPertama => chKedua (convert jd text) => output di main gorutine
 	chPertama := make(chan int, 1)
 	chKedua := make(chan string, 1)
 	passMsg1(chPertama, 2)
@@ -93,13 +101,17 @@ func basicChan() {
 }
 
 func passMsg1(ch chan<- int, data int) {
-	ch <- data
+	fmt.Println("passMsg1", data)
+	ch <- data // accepted
+	// <- ch // error
 }
 
 func passMsg2(ch1 <-chan int, ch2 chan<- string) {
+	fmt.Println("passMsg2")
 	angka := <-ch1
 	texts := []string{"nol", "satu", "dua", "tiga", "empat"}
 	ch2 <- texts[angka]
+	// ch1 <- 2 // error
 }
 
 // fetchURL launch HTTP call GET to url and return response
@@ -198,13 +210,15 @@ func fetchConcurrentlyWaitgroup() {
 	// fmt.Println(start)
 
 	// Create a channel to communicate results from goroutines
-	ch := make(chan string)
+	// must be buffered channel.
+	// ref: https://medium.com/goturkiye/concurrency-in-go-channels-and-waitgroups-25dd43064d1
+	ch := make(chan string, 3)
 	wg := sync.WaitGroup{}
 
 	// Iterate over each URL
 	for _, url := range URLs {
 		// Launch a goroutine to fetch each URL concurrently
-		wg.Add(1)
+		wg.Add(1) // add 1 ke counter
 		go func(url string) {
 			defer wg.Done()
 			result, err := fetchURL(url)
@@ -219,9 +233,11 @@ func fetchConcurrentlyWaitgroup() {
 	}
 
 	var results []string
-	// Collect results from the channel for each URL
-	wg.Wait()
+	wg.Wait() // block until wg "counter" = zero
 
+	// Collect results from the channel for each URL
+
+	close(ch)             // close ch to ensure program can quit loop below
 	for msg := range ch { // iterate len(URLs) times without assign any new variable.
 		results = append(results, msg)
 	}
